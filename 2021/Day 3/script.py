246334542ddfeffe0f5666292390421f59f7f61b
Nick Reilly <nickreilly12@googlemail.com>
2022-12-02 18:58:30 +0000

    .

--- 2021/Day 3/script.py
+++ 2021/Day 3/script.py
@@ -1,9 +1,2 @@
-import csv
-
-itemlist = []
-
-with open('inputs.csv','r') as sourcefile:
-    reader = csv.reader(sourcefile)
-    for row in reader:
-        itemlist.append(row[0])
+import numpy as np
 
