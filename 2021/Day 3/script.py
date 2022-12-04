5419181c7e7e0f63373e031aac17c2413d518366
Nick Reilly <nickreilly12@googlemail.com>
2022-12-04 20:41:53 +0000

    copied a working solution as stuck

--- 2021/Day 3/script.py
+++ 2021/Day 3/script.py
@@ -1,1 +1,20 @@
-import numpy as np
+"""copied from https://github.com/ebouteillon/advent-of-code-2021/blob/main/day-03/part1.py"""
+
+data = open('data', 'r', encoding='utf-8').read().splitlines()
+
+N = len(data[0])
+
+gamma = 0
+epsilon = 0
+
+for n in range(N):
+    count0 = sum(1 for line in data if line[n] == '0')
+    count1 = len(data) - count0
+    gamma *= 2
+    epsilon *= 2
+    if count0 < count1:
+        gamma += 1
+    else:
+        epsilon += 1
+
+print(gamma * epsilon)
