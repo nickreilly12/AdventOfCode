ca0d4eccfdac08a56a9b755fcd2b2463217941e5
Nick Reilly <nickreilly12@googlemail.com>
2022-12-04 21:22:24 +0000

    2021 day 3 activit 1 complete

--- 2021/Day 3/script.py
+++ 2021/Day 3/script.py
@@ -1,2 +1,38 @@
-import numpy as np
-import sys
+gamma = ""
+epsilon = ""
+
+with open('data', 'r') as sourcefile:
+    data = [x for x in sourcefile.read().split()]
+
+N = len(data[0])
+newbinnum = []
+newepsnum = []
+
+for n in range(N):
+    count0 = sum(1 for line in data if line[n] == '0')
+    count1 = len(data) - count0
+    if count0 > count1:
+        newbinnum.append("0")
+        newepsnum.append("1")
+    elif count1 > count0:
+        newbinnum.append("1")
+        newepsnum.append("0")
+
+bitvalue = 2048
+ebitvalue = 2048
+gammanum = 0
+epsilonnum = 0
+for x in newbinnum:
+    if x == "1":
+        gammanum += bitvalue
+    bitvalue /= 2
+
+for x in newepsnum:
+    if x == "1":
+        epsilonnum += ebitvalue
+    ebitvalue /=2
+
+print("Epsilon value is ", epsilonnum)
+print("Gamma value is ", gammanum)
+
+print("Result is ", epsilonnum * gammanum)
