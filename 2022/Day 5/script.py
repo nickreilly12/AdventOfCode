f5f9a9a3babe6e5507ccb66be5029925db886807
Nick Reilly <nickreilly12@googlemail.com>
2022-12-05 09:29:00 +0000

    working on regex to find move information

--- 2022/Day 5/script.py
+++ 2022/Day 5/script.py
@@ -1,5 +1,16 @@
 import numpy as np
+import re
 
-crateposition = np.loadtxt("cratepositions.txt", dtype=str)
+cratepositions = np.loadtxt("cratepositions.txt",dtype=str, delimiter=" ")
 
-print(crateposition)
+with open('moves.txt','r') as sourcefile:
+    moves = [x for x in sourcefile.read().split("\n")]
+
+for x in moves:
+    item = re.search('move(\d+)', x)
+    start = re.search('from(\d+)', x)
+    finish = re.search('to(\d+', x)
+    print(item)
+    print(start)
+    print(finish)
+    import pdb;pdb.set_trace()
