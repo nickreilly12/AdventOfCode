5042fb3a4817cf743153c12c27c6c69f33c457a7
Nick Reilly <nickreilly12@googlemail.com>
2022-12-07 13:34:14 +0000

    Almost there

--- 2022/Day 7/script.py
+++ 2022/Day 7/script.py
@@ -3,15 +3,22 @@ with open('data.txt','r') as sourcefile:
     for x in sourcefile.read().split('\n'):
         rows.append(x)
 
+directory_size = {}
+
 def changedirectory(current,changetype,new):
     if changetype == "..":
         currentlist = []
         for x in current.split('/'):
             currentlist.append(x)
         currentlist.remove(currentlist[len(currentlist)-1])
+        for x in currentlist:
+            if x == "":
+                currentlist.remove(x)
         new = ''
         for f in currentlist:
             new = new+'/'+f
+        if new == '':
+            new = '/'
     elif changetype == "/":
         new = '/'
     else:
@@ -19,15 +26,16 @@ def changedirectory(current,changetype,new):
         for x in current.split('/'):
             currentlist.append(x)
         currentlist.append(new)
-        new = ''
+        #print(currentlist)        
+        for x in currentlist:
+            if x == "":
+                currentlist.remove(x)
         new ="/".join([str(item) for item in currentlist])
-        print(new)
+        if new[0] != "/": new = "/" + new
     return new
 
-def listcontents(current):
-    print("function to list contents")
 def save_sizes_for_directory(current,size):
-    print("combine current directory and sum of all sizes")
+    directory_size[current] = [size]
 
 current = '/'
 
@@ -35,13 +43,23 @@ for row in rows:
     if '$ cd' in row:
         if '$ cd /' in row:
             current = changedirectory(current,'/','/')
-            print(current)
+            #print(current)
         elif '$ cd ..' in row:
             current = changedirectory(current,'..','..')
-            print(current)
-        elif '$ ls' in row:
-            print("show files")
+            #print(current)
         else:
             newdir = row.split(' ')[2]
             current = changedirectory(current,'new',newdir)
-            print(current)
+            #print(current)
+    elif '$ ls' in row:
+        continue
+    elif 'dir' in row:
+        continue
+    else:
+        #this is saving each row individually rather than totalling each file into one value...
+        save_sizes_for_directory(current+'/'+row.split(' ')[1],int(row.split(' ')[0]))
+
+#All files and their sizes have been imported into a single dictionary. I just need to work out how to sum them up...
+       
+for key,value in directory_size.items():
+    print(key,value)
