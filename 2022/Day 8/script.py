a59868507b17c84328580c79ecede284a418cf7e
Nick Reilly <nickreilly12@googlemail.com>
2022-12-08 12:26:40 +0000

    first comparison working

--- 2022/Day 8/script.py
+++ 2022/Day 8/script.py
@@ -1,7 +1,49 @@
-with open('data.txt','r') as sourcefile:
-    data = sourcefile.read().split('\n')
+import numpy as np
 
-treecount = 0
-print(data)
+with open("data.txt",'r') as sourcefile:
+    datalist = [x for x in sourcefile.read().split()]
 
-comparisonlist = []
+totallist = []
+for row in datalist:
+    rowlist = []
+    for n in range(0,99):
+        rowlist.append(row[n])
+    totallist.append(rowlist)
+
+forrest_array = np.asarray(totallist)
+#numpy column
+#print(forrest_array[:,0])
+#numpy row
+#print(forrest_array[0,:])
+
+total_viewable_trees = 0
+
+for row in forrest_array:
+    current_viewable_trees = 1
+    for n in range(0,99):
+        try:
+            if row[n+1] > row[n]:
+                current_viewable_trees +=1
+            elif row[n+1] < row[n]:
+                continue
+        except IndexError:
+            continue
+    total_viewable_trees += current_viewable_trees
+#left to right comparison
+print(total_viewable_trees)
+
+for n in range(0,99):
+    for row in forrest_array:
+        current_viewable_trees = 1
+        for column in row[n]:
+            try:
+                if row[n+1] > row[n]:
+                    current_viewable_trees+=1
+                elif row[n+1] < row[n]:
+                    continue
+            except IndexError:
+                continue
+    total_viewable_trees += current_viewable_trees
+
+#add top to bottom comparison
+print(total_viewable_trees)
