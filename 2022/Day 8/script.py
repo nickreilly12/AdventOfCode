88440f4a7750e25496d693b817222f15ecd9fe15
Nick Reilly <nickreilly12@googlemail.com>
2022-12-08 13:48:41 +0000

    I've been doing it all wrong

--- 2022/Day 8/script.py
+++ 2022/Day 8/script.py
@@ -30,20 +30,23 @@ for row in forrest_array:
             continue
     total_viewable_trees += current_viewable_trees
 #left to right comparison
-print(total_viewable_trees)
+print(f"Total viewable trees after left to right comparison is {total_viewable_trees}")
 
-for n in range(0,99):
-    for row in forrest_array:
-        current_viewable_trees = 1
-        for column in row[n]:
-            try:
-                if row[n+1] > row[n]:
-                    current_viewable_trees+=1
-                elif row[n+1] < row[n]:
-                    continue
-            except IndexError:
+total_viewable_after_top_down = 0
+for column in range(0,99):
+    current_viewable_trees = 1
+    for row in range(0,99):
+        try:
+            if forrest_array[row,column+1] > forrest_array[row,column]:
+                current_viewable_trees += 1
+            elif forrest_array[row,column+1] < forrest_array[row,column]:
                 continue
-    total_viewable_trees += current_viewable_trees
+        except IndexError:
+            continue
+    total_viewable_after_top_down += current_viewable_trees
+total_viewable_trees += total_viewable_after_top_down
+print(f"Current viewable trees after top to bottom comparison is {total_viewable_after_top_down}")
+print(f"This brings the total viewable trees to {total_viewable_trees}")
 
-#add top to bottom comparison
-print(total_viewable_trees)
+#This is giving the same total for top to bottom and left to right comparison...that can't be right.
+#I've done this all wrong...it's only comparing 2 trees at a time - I need to set a view height variable and compare each tree to that...
