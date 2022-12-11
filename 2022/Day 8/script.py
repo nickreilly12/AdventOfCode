8260b6961b0dd8ff7ed6e78b054ab2f9344d19c7
Nick Reilly <nickreilly12@googlemail.com>
2022-12-11 15:24:12 +0000

    I only bloody did it! 2022, day 8 activity 1 complete

--- 2022/Day 8/script.py
+++ 2022/Day 8/script.py
@@ -13,13 +13,83 @@ for row in datalist:
     totallist.append(rowlist)
 
 forrest_array[:,:,0] = np.asarray(totallist)
+forrest_array[:,0,1] = 1
+forrest_array[0,:,1] = 1
+forrest_array[:,-1,1] = 1
+forrest_array[-1,:,1] = 1
 
-current_viewable_height = 0
-Viewable_trees = 0
+for x in range(0,99):
+    current_viewable_height = 0
+    for y in range(0,99):
+        if forrest_array[x,y,1] == 1:
+            if forrest_array[x,y,0] > current_viewable_height:
+                current_viewable_height = forrest_array[x,y,0]
+            continue
+        else:
+            if forrest_array[x,y,0] == current_viewable_height:
+                continue
+            elif forrest_array[x,y,0] > current_viewable_height:
+                forrest_array[x,y,1] = 1
+                current_viewable_height = int(forrest_array[x,y,0])
+            else:
+                continue
 
-current_checklist = []
-for x in forrest_array[:,0,0]:
-    current_checklist.append(x)
+for y in range(0,99):
+    current_viewable_height = 0
+    for x in range(0,99):
+        if forrest_array[x,y,1] == 1:
+            if forrest_array[x,y,0] > current_viewable_height:
+                current_viewable_height = forrest_array[x,y,0]
+            continue
+        else:
+            if forrest_array[x,y,0] == current_viewable_height:
+                continue
+            elif forrest_array[x,y,0] > current_viewable_height:
+                forrest_array[x,y,1] = 1
+                current_viewable_height = int(forrest_array[x,y,0])
+            else:
+                continue
 
-print(current_checklist)
+forrest_array = np.fliplr(forrest_array)
 
+for x in range(0,99):
+    current_viewable_height = 0
+    for y in range(0,99):
+        if forrest_array[x,y,1] == 1:
+            if forrest_array[x,y,0] > current_viewable_height:
+                current_viewable_height = forrest_array[x,y,0]
+            continue
+        else:
+            if forrest_array[x,y,0] == current_viewable_height:
+                continue
+            elif forrest_array[x,y,0] > current_viewable_height:
+                forrest_array[x,y,1] = 1
+                current_viewable_height = int(forrest_array[x,y,0])
+            else:
+                continue
+
+forrest_array = np.flipud(forrest_array)
+
+
+for y in range(0,99):
+    current_viewable_height = 0
+    for x in range(0,99):
+        if forrest_array[x,y,1] == 1:
+            if forrest_array[x,y,0] > current_viewable_height:
+                current_viewable_height = forrest_array[x,y,0]
+            continue
+        else:
+            if forrest_array[x,y,0] == current_viewable_height:
+                continue
+            elif forrest_array[x,y,0] > current_viewable_height:
+                forrest_array[x,y,1] = 1
+                current_viewable_height = int(forrest_array[x,y,0])
+            else:
+                continue
+
+viewable_trees = 0
+
+for x in sum(forrest_array[:,:,1]):
+    viewable_trees += x
+
+print(viewable_trees)
