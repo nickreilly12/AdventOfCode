e7a0df64d71d2f776d0495ff50852c3c5ae12bf9
Nick Reilly <nickreilly12@googlemail.com>
2023-12-22 14:18:04 +0000

    Solution copied from https://www.tinkerassist.com/blog/advent-of-code-2023-day-1-trebuchet

--- 2023/Day 1/task2.py
+++ 2023/Day 1/task2.py
@@ -1,18 +1,24 @@
-help_dict = {
-    'one': '1',
-    'two': '2',
-    'three': '3',
-    'four': '4',
-    'five': '5',
-    'six': '6',
-    'seven': '7',
-    'eight': '8',
-    'nine': '9',
-    'zero': '0'
-}
+file = open("input.txt", "r")
+input = file.read()
 
-with open('converted.txt','w') as sourcefile:
-    for row in sourcefile:
-        result = ''.join(help_dict[ele] for ele in row)
-        print(result)
-        import pdb;pdb.set_trace()
+input_list = input.split('\n')
+
+# part 2
+sum = 0 # create our sum integer
+
+# create list of integers spelled-out in english
+integer_names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+for inp in input_list:
+    nums = [] # craate empty list that we will use to track all integers in list
+    for i, letter in enumerate(inp): # loop through all letters in input line
+        for val, name in enumerate(integer_names): # loop through all spelled-out number options
+            if name in inp[i:i+len(name)]: # if the current spelled-out number we are looking for is found starting at index i, add it to our nums list
+                nums.append(str(val)) # append to our list of integer characters
+        if ord(letter) <= 57: # if unicode value of character is <=57, we know it's an integer
+            nums.append(letter) # append to our list of integer characters
+
+    # concat the first and last characters of our list and add their integer representation to our sum
+    sum += int(nums[0] + nums[-1])
+
+print(sum) # print solution to part 2
